b2e35da5d109ea86f006f758ef6dbaabbf2f7e91
FATIQ HUSSNAIN <fatiqhussnain1@gmail.com>
2023-07-27 07:23:26 -0700

    making new activities and testing new themes

--- app/src/main/java/com/example/androidthemes/MainActivity.kt
+++ app/src/main/java/com/example/androidthemes/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.androidthemes
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     private lateinit var button: Button
@@ -19,4 +22,48 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.example_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            R.id.item1 -> {
+                Toast.makeText(this, "Item 1 selected", Toast.LENGTH_SHORT).show()
+                return true
+            }
+
+            R.id.item2 -> {
+                Toast.makeText(this, "Item 2 selected", Toast.LENGTH_SHORT).show()
+                return true
+            }
+
+            R.id.item3 -> {
+                Toast.makeText(this, "Item 3 selected", Toast.LENGTH_SHORT).show()
+                return true
+            }
+
+            R.id.nested_item_1 -> {
+                Toast.makeText(this, "Nested_Item 1 selected", Toast.LENGTH_SHORT).show()
+                return true
+            }
+
+            R.id.nested_item_2 -> {
+                Toast.makeText(this, "Nested_Item 2 selected", Toast.LENGTH_SHORT).show()
+                return true
+            }
+
+            else -> {
+                return super.onOptionsItemSelected(item)
+            }
+
+        }
+
+
+    }
+
 }
--- app/src/main/java/com/example/androidthemes/ThirdActivity.kt
+++ app/src/main/java/com/example/androidthemes/ThirdActivity.kt
@@ -5,18 +5,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
-class SecondActivity : AppCompatActivity() {
+class ThirdActivity : AppCompatActivity() {
+
     private lateinit var button: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
+        setContentView(R.layout.activity_third)
 
-        button = findViewById(R.id.button2)
-
-
+        button = findViewById(R.id.button3)
 
         button.setOnClickListener {
-            val intent = Intent(this, ThirdActivity::class.java)
+            val intent = Intent(this, SecondActivity::class.java)
             startActivity(intent)
         }
     }
